bb8021418d0a82b5381d54f3062ce7514f17eb4b
toseefkhilji <toseefkhilji@gmail.com>
2023-09-28 15:48:17 +0530

    public fixed

--- Sources/Loaf/Loaf.swift
+++ Sources/Loaf/Loaf.swift
@@ -261,7 +261,7 @@ protocol LoafDelegate: AnyObject {
     func loafDidDismiss()
 }
 
-final public class LoafViewController: UIViewController {
+final class LoafViewController: UIViewController {
     var loaf: Loaf
     
     let label = UILabel()
